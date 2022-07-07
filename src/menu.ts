015cda67f86596b4167659ae432352da8d40142d
Marina <marinafullferrer@gmail.com>
2022-07-07 16:47:30 +0200

    Programa finalizado

--- src/menu.ts
+++ src/menu.ts
@@ -41,13 +41,19 @@ export async function gestionarOpcion(
         await rlp.questionAsync('');   
     }
     else if(opcion === '5') {
-        console.log('Opción 5');    
+        console.clear();
+        await teatro.cancelarLocalidad(rlp)
+        await rlp.questionAsync('');   
     }
     else if(opcion === '6') {
-        console.log('Opción 6');    
+        console.clear();
+        await teatro.consultarLocalidad(rlp);
+        await rlp.questionAsync('');  
     }
     else if(opcion === '7') {
-        console.log('Opción 7');    
+        console.clear();
+        teatro.calcularRecaudacion();
+        await rlp.questionAsync(''); 
     }
     else if(opcion === '8') {
         console.log('Opción 8');    
